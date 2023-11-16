4fe4574c9bf7979ef648e126b1109f1b2815729a
Markymy <rocabertemarky09@gmail.com>
2023-11-16 19:39:55 +0800

    Activity 6

--- app/src/main/java/com/patigayon/activity6/MainActivity.kt
+++ app/src/main/java/com/patigayon/activity6/MainActivity.kt
@@ -15,58 +15,56 @@ class MainActivity : AppCompatActivity() {
     private lateinit var timePickerSelection: TextView
     private lateinit var customDialogSelection: TextView
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            val buttonClick = AnimationUtils.loadAnimation(this, R.anim.button_click)
+        val buttonClick = AnimationUtils.loadAnimation(this, R.anim.button_click)
 
-            alertDialogSelection = findViewById(R.id.alertDialogSelection)
+        alertDialogSelection = findViewById(R.id.alertDialogSelection)
+        datePickerSelection = findViewById(R.id.datePickerSelection)
+        timePickerSelection = findViewById(R.id.timePickerSelection)
+        customDialogSelection = findViewById(R.id.customDialogSelection)
 
-            alertDialogSelection = findViewById(R.id.alertDialogSelection)
-            datePickerSelection = findViewById(R.id.datePickerSelection)
-            timePickerSelection = findViewById(R.id.timePickerSelection)
-            customDialogSelection = findViewById(R.id.customDialogSelection)
+        val alertDialogButton = findViewById<Button>(R.id.alertDialogButton)
+        val datePickerDialogButton = findViewById<Button>(R.id.datePickerDialogButton)
+        val timePickerDialogButton = findViewById<Button>(R.id.timePickerDialogButton)
+        val customDialogButton = findViewById<Button>(R.id.customDialogButton)
 
-
-            val alertDialogButton = findViewById<Button>(R.id.alertDialogButton)
-            alertDialogButton.setOnClickListener {
-                it.startAnimation(buttonClick)
-                showAlertDialog()
-            }
-
-            val datePickerDialogButton = findViewById<Button>(R.id.datePickerDialogButton)
-            datePickerDialogButton.setOnClickListener {
-                it.startAnimation(buttonClick)
-                showDatePickerDialog()
-            }
-
-            val timePickerDialogButton = findViewById<Button>(R.id.timePickerDialogButton)
-            timePickerDialogButton.setOnClickListener {
-                it.startAnimation(buttonClick)
-                showTimePickerDialog()
-            }
-
-            val customDialogButton = findViewById<Button>(R.id.customDialogButton)
-            customDialogButton.setOnClickListener {
-                it.startAnimation(buttonClick)
-                showCustomDialog()
-            }
+        alertDialogButton.setOnClickListener {
+            it.startAnimation(buttonClick)
+            showAlertDialog()
         }
+
+        datePickerDialogButton.setOnClickListener {
+            it.startAnimation(buttonClick)
+            showDatePickerDialog()
+        }
+
+        timePickerDialogButton.setOnClickListener {
+            it.startAnimation(buttonClick)
+            showTimePickerDialog()
+        }
+
+        customDialogButton.setOnClickListener {
+            it.startAnimation(buttonClick)
+            showCustomDialog()
+        }
+    }
 
     private fun showAlertDialog() {
         val editText = EditText(this)
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Alert")
-        builder.setMessage("This is an alert dialog.")
+        builder.setTitle(getString(R.string.alert_dialog_title))
+        builder.setMessage(getString(R.string.alert_dialog_message))
         builder.setView(editText)
-        builder.setPositiveButton("OK") { dialog, which ->
+        builder.setPositiveButton(getString(R.string.alert_dialog_ok)) { _, _ ->
             val userInput = editText.text.toString()
-            alertDialogSelection.text = "Alert response: $userInput"
-            Toast.makeText(applicationContext, "Pressed OK!", Toast.LENGTH_SHORT).show()
+            alertDialogSelection.text = getString(R.string.alert_dialog_user_input, userInput)
+            Toast.makeText(applicationContext, getString(R.string.pressed_ok), Toast.LENGTH_SHORT).show()
         }
-        builder.setNegativeButton("Cancel") { dialog, which ->
-            Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT).show()
+        builder.setNegativeButton(getString(R.string.alert_dialog_cancel)) { _, _ ->
+            Toast.makeText(applicationContext, getString(R.string.cancelled), Toast.LENGTH_SHORT).show()
         }
         builder.show()
     }
@@ -78,8 +76,8 @@ class MainActivity : AppCompatActivity() {
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
         val dpd = DatePickerDialog(this, { _, selectedYear, monthOfYear, dayOfMonth ->
-            val dateMsg = "$dayOfMonth/${monthOfYear + 1}/$selectedYear"
-            datePickerSelection.text = "Date chosen: $dateMsg"
+            val dateMsg = resources.getString(R.string.date_picker_title, "$dayOfMonth/${monthOfYear + 1}/$selectedYear")
+            datePickerSelection.text = dateMsg
             Toast.makeText(applicationContext, dateMsg, Toast.LENGTH_SHORT).show()
         }, year, month, day)
 
@@ -92,8 +90,8 @@ class MainActivity : AppCompatActivity() {
         val minute = calendar.get(Calendar.MINUTE)
 
         val tpd = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
-            val timeMsg = "$selectedHour:$selectedMinute"
-            timePickerSelection.text = "Time chosen: $timeMsg"
+            val timeMsg = resources.getString(R.string.time_picker_title, "$selectedHour:$selectedMinute")
+            timePickerSelection.text = timeMsg
             Toast.makeText(applicationContext, timeMsg, Toast.LENGTH_SHORT).show()
         }, hour, minute, true)
 
@@ -105,15 +103,15 @@ class MainActivity : AppCompatActivity() {
         val customDialogEditText = dialogView.findViewById<EditText>(R.id.customDialogEditText)
 
         val dialog = AlertDialog.Builder(this)
-            .setTitle("Custom Dialog")
+            .setTitle(getString(R.string.custom_dialog_title))
             .setView(dialogView)
             .create()
 
         dialogView.findViewById<Button>(R.id.customDialogButton).setOnClickListener {
             val userInput = customDialogEditText.text.toString()
-            customDialogSelection.text = "Custom dialog input: $userInput"
+            customDialogSelection.text = getString(R.string.custom_dialog_input_text, userInput)
             dialog.dismiss()
-            Toast.makeText(applicationContext, "Submitted: $userInput", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, getString(R.string.custom_dialog_input_text, userInput), Toast.LENGTH_SHORT).show()
         }
 
         dialog.show()
